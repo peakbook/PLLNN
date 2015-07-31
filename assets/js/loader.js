5d21b6b396dbf4a0a98e0489466941d446faf187
PeakbooK <peakbook@users.noreply.github.com>
2015-07-31 22:03:41 +0900

    update

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,9 +1,6 @@
 if (typeof window.fx === 'undefined') {
     var oldScript = document.querySelector('script[type="application/javascript;version=1.7"]');
-    var text = oldScript.text;
     document.body.removeChild(oldScript);
-
-    var newScript = document.createElement('script');
-    newScript.text = text;
-    document.body.appendChild(newScript);
+    oldScript.type="text/javascript";
+    document.body.appendChild(oldScript);
 }
